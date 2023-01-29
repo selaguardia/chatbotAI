6bdede6a29ed86eeb7b5a99c6978de4e0d4bee9b
selaguardia <selaguardia91@gmail.com>
2023-01-28 16:15:19 -0800

    install nodemon

--- client/script.js
+++ client/script.js
@@ -81,7 +81,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   // fetch data from server = bots response
-  const response = await fetch("http://localhost:5000", {
+  const response = await fetch("https://chatbotai-dqdd.onrender.com", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
